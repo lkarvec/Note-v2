332d1bb03c12ee70bdfae59c2609fc2b74eeb589
MagicShmagic <35780303+MagicShmagic@users.noreply.github.com>
2019-04-10 11:02:59 -0500

    Main class upload, toss names in the @author tag.

---
+++ MusicalNoteTrainer/src/applicaiton/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 	@author Christopher Tyler (tbl512)
+ * 	UTSA CS 3443 - Final Project
+ * 	Spring 2019
+ */
+
+/**
+ *  Toss some names under the author tag, I'll get to posting other stuff. 
+ * - Christopher
+ */
+
+package applicaiton;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
+
